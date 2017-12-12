094fe08007191496b151121e74c91cd3cbdef252
13166253532 <13166253532@163.com>
2017-12-12 10:00:06 +0800

    init

---
+++ GotTop/GotTop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GotTop
+//
+//  Created by xiejiangbo on 2017/12/12.
+//  Copyright © 2017年 yin chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GotTop/GotTop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GotTop
+//
+//  Created by xiejiangbo on 2017/12/12.
+//  Copyright © 2017年 yin chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
